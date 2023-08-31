d6536075e443b9427b805c5306e8cec6f8b6f1f2
rehanvandermerwe <rehan.merwe@messagebird.com>
2023-08-31 19:51:12 +0200

    fix: cognito callback url must have query string params

--- src/src/frontend/src/views/login_callback.vue
+++ src/src/frontend/src/views/login_callback.vue
@@ -10,11 +10,22 @@ import router from '@frontend/src/router';
 
 onMounted(() => {
   const idToken = new URLSearchParams(window.location.hash).get('#id_token');
+  let state = new URLSearchParams(window.location.href).get('state');
+  let queryObject: Record<string, string> | undefined = undefined;
+  if(state)
+  {
+      state = decodeURIComponent(state);
+      queryObject = state.split("&").reduce((acc: Record<string, string>, pair) => {
+          const [key, value] = pair.split("=");
+          acc[key] = value;
+          return acc;
+      }, {});
+  }
   if(idToken)
   {
     const systemStore = getSystemStore();
     systemStore.$patch({apiJwtToken: idToken});
-    router.push({name: 'page_stats'});
+    router.push({name: 'page_stats', query: queryObject});
   }
 
 });
